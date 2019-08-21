3f5e0caca809d6358cdf0980deb2db9d57df8af7
Idan Hershcovich <idanhershcovich@n3-30-108.dhcp.drexel.edu>
2019-08-21 13:39:18 -0400

    Added nick's changes

--- dev/checkmate-front-end/src/App.js
+++ dev/checkmate-front-end/src/App.js
@@ -10,10 +10,18 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state={
-      loginPage:[],
-      uploadScreen:[]
-    }
+      // loginPage:[],
+      // uploadScreen:[]
+      isAuthenticated:  false
+    };
   }
+
+  userHasAuthenticated = authenticated => {
+    this.setState({
+      isAuthenticated : authenticated
+    });
+  }
+
   // componentWillMount(){
   //   var loginPage =[];
   //   loginPage.push(<LoginScreen appContext={this} key={"login-screen"}/>);
@@ -22,6 +30,11 @@ class App extends Component {
   //                   })
   // }
   render() {
+
+    const childProps = {
+      isAuthenticated: this.state.isAuthenticated,
+      userHasAuthenticated: this.userHasAuthenticated
+    };
     return (
       <div className="App">
       <Link to="/">Home Page</Link>
@@ -29,6 +42,7 @@ class App extends Component {
       <Link to="/register">Register</Link>
       <Link to="/game">Board</Link>
       <Routes/>
+      <Routes childProps= {childProps}/>
       </div>
    
     );
