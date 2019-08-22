d4b2f98a2691fbd7e72f96f2b05306d2440eebe9
Idan Hershcovich <idanhershcovich@n3-30-108.dhcp.drexel.edu>
2019-08-22 17:58:33 -0400

    Added working websocket messages using a dummy NodeJS server

--- dev/checkmate-front-end/src/components/ChatBox.js
+++ dev/checkmate-front-end/src/components/ChatBox.js
@@ -1,14 +1,15 @@
-import '../Game.css'
-import React, { Component } from 'react';
+// import '../Game.css'
+// import React, { Component } from 'react';
+// import Chat from './Chat/Chat'
 
-class ChatBox extends Component {
-    render() {
-        return(
-            <div className="chatBox">
-              
-            </div>
-        );
-    }
-}
+// class ChatBox extends Component {
+//     render() {
+//         return(
+//             <div className="chatBox">
+//                <Chat/>
+//             </div>
+//         );
+//     }
+// }
 
-export default ChatBox;
+// export default ChatBox;
