4f3022f8c88bcba30be6f4340a9135536f217edb
Idan Hershcovich <idanhershcovich@n3-30-108.dhcp.drexel.edu>
2019-08-27 12:31:35 -0400

    Bug Fix: Local storage was not updated when changing routes

--- dev/checkmate-front-end/src/containers/Home.js
+++ dev/checkmate-front-end/src/containers/Home.js
@@ -1,7 +1,11 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import "./Home.css"
+import {NavItem} from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 export default class Home extends Component {
+  
+ 
   render() {
       
     return (
@@ -10,28 +14,41 @@ export default class Home extends Component {
             <h1>CheckMate</h1>
             <p>A simple Checkers app</p>
           </div>
-      
-          <div className="centerMenu">
-            <div class="col-md-3 col-sm-6 hero-feature">
-            <div class="thumbnail">
-                <img src="https://icon-library.net/images/multiple-people-icon/multiple-people-icon-10.jpg" alt="" className="publicImage"/>
-                <div class="caption">
-                    <h3>Public Game</h3>
-                    <p>Jump on queue with random players!</p>
-                </div>
-            </div>
-            </div>
+            <div className="centerMenu">  
+              {window.localStorage.getItem("username") ?
+                <Fragment>
+                  <div class="col-md-3 col-sm-6 hero-feature">
+                    <div class="thumbnail">
+                    <LinkContainer to="/game"> 
+                      <NavItem>
+                        <img src="https://icon-library.net/images/multiple-people-icon/multiple-people-icon-10.jpg" alt="" className="publicImage"/>
+                        <div class="caption">
+                            <h3>Public Game</h3>
+                            <p>Jump on queue with random players!</p>
+                        </div>
+                      </NavItem>
+                      </LinkContainer>
+                    </div>
+                  </div>
 
-            <div class="col-md-3 col-sm-6 hero-feature">
-            <div class="thumbnail">
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYHVOKAjjrBHSCPiEqGoscxboPP6s_fGK5NFUk6JZJJ5LcAXzE" alt="" className="privateImage"/>
-                <div class="caption">
-                    <h3>Private Game</h3>
-                    <p>Share your unique link with a friend and play!</p>
+                
+                <div class="col-md-3 col-sm-6 hero-feature">
+                  <div class="thumbnail">
+                  <LinkContainer to="/game"> 
+                    <NavItem>
+                      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYHVOKAjjrBHSCPiEqGoscxboPP6s_fGK5NFUk6JZJJ5LcAXzE" alt="" className="privateImage"/>
+                      <div class="caption">
+                          <h3>Private Game</h3>
+                          <p>Share your unique link with a friend and play!</p>
+                      </div>
+                    </NavItem>
+                  </LinkContainer>
+                  </div>
                 </div>
+                </Fragment> :
+                <div></div>
+              }
             </div>
-            </div>
-          </div>
         </div>
         
     );
